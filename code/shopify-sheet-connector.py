dddac9d52e7eba6ed8b42cb18fc1559b727602c9
BSL5 <biosafetylevel5@gmail.com>
2020-09-10 10:08:14 -0700

    Updating code; cards

--- code/shopify-sheet-connector.py
+++ code/shopify-sheet-connector.py
@@ -1,8 +1,11 @@
 from utils import *
 import re
 import numpy as np
+import shopify
+from pyactiveresource.connection import ResourceNotFound
 
 SPREADSHEET_ID = "1LZCXzBtgey9xv5OH7YGYgp8UMJ27Eyj1aF9IhAW6M6o"
+
 
 data = getShopifyProductData()
 shopify.CarrierService.find()
@@ -14,7 +17,7 @@ fedexInfo = getSheet(SPREADSHEET_ID, "FedEx Info", useFirstRowAsCols=True)
 fedexInfo.rename(columns={"Handle": "handle", "Length": "length", "Width": "width", "Height": "height",
                           "Country Of Manufacture": "country_of_manufacture"}, inplace=True)
 df = df.merge(fedexInfo[["length", "height", "width", "country_of_manufacture", "handle"]], right_on="handle",
-              left_on="handle")
+              left_on="handle", how="left")
 
 df.rename(columns={"body_html": "body"}, inplace=True)
 
@@ -27,7 +30,7 @@ for col in addCols:
 
 
 # cells can only have 50000 chars in them
-df = splitColsViaCharMax(df, charMax=49999)
+    df = splitColsViaCharMax(df, charMax=49999)
 
 columnOrder = sorted(df.columns)
 columnOrder.remove("title")
@@ -35,7 +38,10 @@ columnOrder.remove("handle")
 columnOrder = ["title", "handle"] + columnOrder
 df = df[columnOrder]
 
-#updateSheet(df.applymap(str), SPREADSHEET_ID, "Product Information")
+dff = df.applymap(str)
+
+#dff = dff.applymap(lambda x: x[0:40000] if len(x)>40000 else x)
+updateSheet(dff, SPREADSHEET_ID, "Product Information")
 
 df = recombineSplitColumns(df)
 
@@ -43,8 +49,12 @@ packaging = getSheet(SPREADSHEET_ID, "Packaging", useFirstRowAsCols=True)
 collections = getSheet(SPREADSHEET_ID, "Collections", useFirstRowAsCols=True)
 
 for i, row in df.iterrows():
-    product = shopify.Product.find(row["id"])
-    packages = packaging[packaging["id"].astype(int) == int(row["id"])]
+    try:
+        product = shopify.Product.find(row["id"])
+    except ResourceNotFound as e:
+        print(f"!!! ERROR   !!! Could not find any matches for {row['title']} with an id of {row['id']} in packaging. Error: {e}")
+        continue
+    packages = packaging[packaging["id"].replace("", -1).astype(int) == int(row["id"])]
     if not len(packages) == 1:
         print(f"!!! WARNING !!! There are {len(packages)} matches for {row['title']} with an id of {row['id']} in packaging.")
         continue
@@ -106,14 +116,14 @@ for i, row in df.iterrows():
     geneInfo["gene_name_short"] = "<a target='_freegenes' href='http://" + geneInfo["trading_card_link"] +"'>" + geneInfo["gene_name_short"] +"</a>"
     geneInfo = geneInfo[["gene_name_short", "gene_name_long", "genbank_protein_id"]]
     geneInfo.rename(columns={"gene_name_short":"Gene", "gene_name_long":"Name", "genbank_protein_id":"NCBI ID"}, inplace=True)
-    geneInfo.fillna(value=np.nan, inplace=True)
-
+    
+    geneInfo = geneInfo.replace("", np.nan).dropna(how="all", axis=1).dropna(how="all").fillna(value=np.nan)
     table = geneInfo.to_html(index=False, index_names=False, header=True, escape=False,
         na_rep="", formatters={"NCBI ID": lambda id: f"<a target='_NCBI' href='https://www.ncbi.nlm.nih.gov/protein/{id}'>{id}</a>"})
 
     product.body_html = re.sub(f"{geneStart}.*?{geneEnd}", geneStart+table+geneEnd, product.body_html, flags=re.DOTALL)
     status = product.save()
-    if status == True:
+    if status:
         print(f"Pushed genes for product {row['title']}")
     else:
         print(f"Encountered issues when pushing genes for product {row['title']}")
